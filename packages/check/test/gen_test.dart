55ff832b7a9f201db091172a791def752324b6d6
Ian McIntosh <cranston.ian@gmail.com>
2023-10-16 11:54:25 -0400

    Dependency updates.

--- packages/check/test/gen_test.dart
+++ packages/check/test/gen_test.dart
@@ -3,7 +3,7 @@ import 'package:ribs_check/src/prop.dart';
 import 'package:test/expect.dart';
 
 void main() {
-  forAll(Gen.dateTime)((x) {
+  forAll('forAll', Gen.dateTime)((x) {
     expect(x.year, isPositive);
-  }).run(description: 'forAll', numTests: 500);
+  }).run();
 }
