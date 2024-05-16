c1a9ca9054286aa2ca250427d5648b8090c92d6d
Ian McIntosh <cranston.ian@gmail.com>
2024-05-16 09:38:17 -0400

    Add CyclicBarrier

---
+++ packages/effect/lib/src/std/cyclic_barrier.dart
@@ -0,0 +1,72 @@
+import 'package:ribs_core/ribs_core.dart';
+import 'package:ribs_effect/ribs_effect.dart';
+
+final class CyclicBarrier {
+  final int capacity;
+  final Ref<_State> _state;
+
+  static IO<CyclicBarrier> withCapacity(int capacity) {
+    if (capacity < 1) {
+      throw ArgumentError(
+          'Cyclic barrier constructed with capacity $capacity. Must be > 0');
+    }
+
+    return IO
+        .deferred<Unit>()
+        .map((unblock) => _State(capacity, 0, unblock))
+        .flatMap(Ref.of)
+        .map((state) => CyclicBarrier._(capacity, state));
+  }
+
+  CyclicBarrier._(this.capacity, this._state);
+
+  IO<Unit> await() {
+    return IO.deferred<Unit>().flatMap((gate) {
+      return _state.flatModifyFull((tuple) {
+        final (poll, prev) = tuple;
+
+        final awaitingNow = prev.awaiting - 1;
+
+        if (awaitingNow == 0) {
+          return (
+            _State(capacity, prev.epoch + 1, gate),
+            prev.unblock.complete(Unit()).voided()
+          );
+        } else {
+          final newState = _State(awaitingNow, prev.epoch, prev.unblock);
+
+          // reincrement count if this await gets canceled,
+          // but only if the barrier hasn't reset in the meantime
+          final cleanup = _state.update((s) {
+            if (s.epoch == prev.epoch) {
+              return s.copy(awaiting: s.awaiting + 1);
+            } else {
+              return s;
+            }
+          });
+
+          return (newState, poll(prev.unblock.value()).onCancel(cleanup));
+        }
+      });
+    });
+  }
+}
+
+final class _State {
+  final int awaiting;
+  final int epoch;
+  final Deferred<Unit> unblock;
+
+  const _State(this.awaiting, this.epoch, this.unblock);
+
+  _State copy({
+    int? awaiting,
+    int? epoch,
+    Deferred<Unit>? unblock,
+  }) =>
+      _State(
+        awaiting ?? this.awaiting,
+        epoch ?? this.epoch,
+        unblock ?? this.unblock,
+      );
+}
---
+++ packages/effect/test/std/cyclic_barrier_test.dart
@@ -0,0 +1,78 @@
+import 'package:ribs_core/ribs_core.dart';
+import 'package:ribs_effect/ribs_effect.dart';
+import 'package:ribs_effect/src/std/cyclic_barrier.dart';
+import 'package:ribs_effect/test_matchers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Cyclic Barrier', () {
+    IO<CyclicBarrier> newBarrier(int n) => CyclicBarrier.withCapacity(n);
+
+    test('await is blocking', () {
+      final test = newBarrier(2).flatMap((barrier) => barrier.await());
+
+      expect(test, ioSucceeded());
+    }, skip: 'Expected to be non-terminating');
+
+    test('await is cancelable', () {
+      final test = newBarrier(2)
+          .flatMap((barrier) => barrier.await())
+          .timeoutTo(1.second, IO.unit);
+
+      expect(test, ioSucceeded());
+    });
+
+    test('await releases all fibers', () {
+      final test = newBarrier(2).flatMap((barrier) {
+        return (barrier.await(), barrier.await()).parTupled().voided();
+      });
+
+      expect(test, ioSucceeded());
+    });
+
+    test('should reset once full', () {
+      final test = newBarrier(2).flatMap((barrier) {
+        return (barrier.await(), barrier.await())
+            .parTupled()
+            .productR(() => barrier.await());
+      });
+
+      expect(test, ioSucceeded());
+    }, skip: 'Expected to be non-terminating');
+
+    test('should clean up upon cancelation of await', () {
+      final test = newBarrier(2).flatMap((barrier) {
+        return barrier
+            .await()
+            .timeoutTo(1.second, IO.unit)
+            .productR(() => barrier.await());
+      });
+
+      expect(test, ioSucceeded());
+    }, skip: 'Expected to be non-terminating');
+
+    test('barrier of capacity 1 is a no op', () {
+      expect(
+        newBarrier(1).flatMap((barrier) => barrier.await()),
+        ioSucceeded(),
+      );
+    });
+
+    test('race fiber cancel and barrier full', () {
+      const iterations = 100;
+
+      final run = newBarrier(2).flatMap((barrier) {
+        return barrier.await().start().flatMap((fiber) {
+          return IO.race(barrier.await(), fiber.cancel()).flatMap((result) {
+            return result.fold(
+              (_) => (barrier.await(), barrier.await()).parTupled().voided(),
+              (_) => IO.unit,
+            );
+          });
+        });
+      });
+
+      expect(IList.fill(iterations, run).sequence(), ioSucceeded());
+    });
+  });
+}
