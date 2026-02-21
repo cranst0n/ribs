444caf99101a53feb27b43c8c9c2975558341d48
Ian McIntosh <cranston.ian@gmail.com>
2026-02-20 22:28:54 -0500

    Add a few benchmarks.

--- packages/bench/lib/rill/basic/rill.dart
+++ packages/bench/lib/rill/basic/rill.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: avoid_print
 
+import 'package:ribs_core/ribs_core.dart';
 import 'package:ribs_rill/ribs_rill.dart';
 
-const nElements = 10000000;
+final nElements = Integer.MaxValue;
 
+// dart compile exe -o /tmp/rill_stack_overflow packages/bench/lib/rill/basic/rill.dart
 void main(List<String> args) async {
-  final rill = Rill.range(0, nElements).map((x) => x * 2).filter((x) => x % 4 == 0);
+  final rill = Rill.range(0, nElements).map((x) => x * 2).filter((x) => x % 3 == 0).take(50000000);
   await rill.compile.last.unsafeRunFuture();
 }
--- packages/bench/lib/rill/basic/stream.dart
+++ packages/bench/lib/rill/basic/stream.dart
@@ -2,7 +2,10 @@ const nElements = 10000000;
 
 void main(List<String> args) async {
   final stream = Stream.fromIterable(
-    Iterable.generate(nElements, (i) => i).map((x) => x * 2).where((x) => x % 3 == 0),
+    Iterable.generate(
+      nElements,
+      (i) => i,
+    ).map((x) => x * 2).where((x) => x % 3 == 0).take(50000000),
   );
 
   await for (final _ in stream) {}
