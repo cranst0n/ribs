0b2cfc223ad66f8a8f5cf70f6cf8fb0b4c69caa0
Ian McIntosh <cranston.ian@gmail.com>
2026-02-21 18:18:26 -0500

    Rewrite binary ListCodec to be stack safe.

--- packages/binary/lib/src/codecs/list_codec.dart
+++ packages/binary/lib/src/codecs/list_codec.dart
@@ -9,20 +9,23 @@ final class ListCodec<A> extends Codec<List<A>> {
 
   @override
   Either<Err, DecodeResult<List<A>>> decode(BitVector bv) {
-    Either<Err, DecodeResult<List<A>>> go(BitVector x, List<A> acc) {
-      if (x.isEmpty) {
-        return Either.right(DecodeResult(acc, x));
-      } else {
-        return limit
-            .filterNot((l) => acc.length >= l)
-            .fold(
-              () => Either.right(DecodeResult(acc, x)),
-              (limit) => codec.decode(x).flatMap((a) => go(a.remainder, acc..add(a.value))),
-            );
-      }
-    }
+    var currentBV = bv;
+    final acc = List<A>.empty(growable: true);
 
-    return go(bv, List.empty(growable: true));
+    while (true) {
+      if (currentBV.isEmpty || limit.exists((l) => acc.length >= l)) {
+        return Either.right(DecodeResult(acc, currentBV));
+      }
+
+      final result = codec.decode(currentBV);
+      if (result.isLeft) {
+        return Either.left(result.swap().getOrElse(() => throw StateError('unreachable')));
+      }
+
+      final r = result.getOrElse(() => throw StateError('unreachable'));
+      acc.add(r.value);
+      currentBV = r.remainder;
+    }
   }
 
   @override
