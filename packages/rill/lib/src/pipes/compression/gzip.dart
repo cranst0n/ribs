c70392b344673aa32c0a05d05b352c060c1c1e5a
Ian McIntosh <cranston.ian@gmail.com>
2026-04-30 22:03:58 -0400

    API docs for rill package.

--- packages/rill/lib/src/pipes/compression/gzip.dart
+++ packages/rill/lib/src/pipes/compression/gzip.dart
@@ -1,6 +1,14 @@
 import 'package:ribs_rill/ribs_rill.dart';
 import 'package:ribs_rill/src/pipes/compression/gzip_platform/gzip_platform.dart';
 
+/// GZip compression and decompression pipes.
+///
+/// Obtained via [Pipes.compression].gzip or `GZipPipes()`.
+///
+/// ```dart
+/// final compressed = rawBytes.through(Pipes.compression.gzip.encode);
+/// final decompressed = compressed.through(Pipes.compression.gzip.decode);
+/// ```
 class GZipPipes {
   static final GZipPipes _singleton = GZipPipes._();
 
@@ -10,7 +18,9 @@ class GZipPipes {
 
   static final _platform = GZipPlatform();
 
+  /// Decompresses a GZip-compressed stream of raw bytes.
   Pipe<int, int> get decode => _platform.decode;
 
+  /// Compresses a stream of raw bytes using GZip.
   Pipe<int, int> get encode => _platform.encode;
 }
--- packages/rill/lib/src/pipes/compression/gzip_platform/gzip_platform.dart
+++ packages/rill/lib/src/pipes/compression/gzip_platform/gzip_platform.dart
@@ -2,10 +2,13 @@ import 'package:ribs_rill/ribs_rill.dart';
 import 'package:ribs_rill/src/pipes/compression/gzip_platform/gzip_platform_stub.dart'
     if (dart.library.io) 'package:ribs_rill/src/pipes/compression/gzip_platform/gzip_platform_io.dart';
 
+/// Platform abstraction for GZip compress/decompress operations.
 abstract class GZipPlatform {
   factory GZipPlatform() => GZipPlatformImpl();
 
+  /// Decompresses a GZip-compressed stream of raw bytes.
   Pipe<int, int> get decode;
 
+  /// Compresses a stream of raw bytes using GZip.
   Pipe<int, int> get encode;
 }
